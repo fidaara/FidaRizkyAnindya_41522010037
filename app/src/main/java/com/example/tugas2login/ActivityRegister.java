56bf225053e9c17b0f9eba45039bc2d3ba96d41a
fidaara <41522010037@student.mercubuana.ac.id>
2024-04-24 12:12:53 +0700

    Update terbaru

--- app/src/main/java/com/example/tugas2login/ActivityRegister.java
+++ app/src/main/java/com/example/tugas2login/ActivityRegister.java
@@ -1,6 +1,9 @@
 package com.example.tugas2login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ActivityRegister extends AppCompatActivity {
+
+    Button masukkelogin;
+
+    Button masukkeregis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,27 @@ public class ActivityRegister extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        masukkelogin = findViewById(R.id.btnlogin);
+
+
+        masukkelogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent masuk = new Intent(ActivityRegister.this, ActivityLogin.class);
+                startActivity(masuk);
+            }
+        });
+
+        masukkeregis = findViewById(R.id.btnregis);
+
+        masukkeregis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent masukreg = new Intent(ActivityRegister.this, ActivityRegis.class);
+                startActivity(masukreg);
+            }
         });
     }
 }
