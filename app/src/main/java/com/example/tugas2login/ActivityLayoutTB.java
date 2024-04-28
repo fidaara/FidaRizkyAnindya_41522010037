2ec371e498f16860cc28a184cfc7146e36c061f2
fidaara <41522010037@student.mercubuana.ac.id>
2024-04-28 21:35:57 +0700

    Update terbaru

--- app/src/main/java/com/example/tugas2login/ActivityLayoutTB.java
+++ app/src/main/java/com/example/tugas2login/ActivityLayoutTB.java
@@ -1,9 +1,6 @@
 package com.example.tugas2login;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,29 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ActivityLogin extends AppCompatActivity {
-
-    Button masukkeuser;
+public class ActivityLayoutTB extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_layout_tb);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        masukkeuser = findViewById(R.id.imgrectangle481);
-
-        masukkeuser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent masukkeuser = new Intent(ActivityLogin.this, ActivityLayoutTB.class);
-                startActivity(masukkeuser);
-            }
         });
     }
 }
